9a5e5d2257652097e203b3ca31314d1935afd762
kuuhaku-u <ryusama6996@gmail.com>
2022-10-27 15:03:00 +0530

    points system

--- nodets/src/models/signup/index.ts
+++ nodets/src/models/signup/index.ts
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 interface IUser {
   firstName: string;
   passWord: string;
+  userPoints:number
 }
 const signup = new Schema<IUser>({
   firstName: { type: String, required: true },
   passWord: { type: String, required: true },
+  userPoints :{type:Number, default : 0}
 });
 const Signup = mongoose.model("user", signup);
 export default Signup
--- nodets/src/routes/questions/index.ts
+++ nodets/src/routes/questions/index.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
 import { Types } from 'mongoose';
 import Question from '../../models/questions';
+import Signup from '../../models/signup';
 import Tags from '../../models/tags';
-import upvote from '../../models/upvotes';
 const QuestionRoute = express.Router();
 QuestionRoute.post(
   '/question',
@@ -85,15 +85,74 @@ QuestionRoute.post(
       const checking = await Question.findOne({
         _id: questionID,
       });
+      const userToFound: any = await Signup.findOne({
+        _id: userId,
+      });
       if (checking) {
+        let point: number = userToFound.userPoints + 1;
+        //  point >= 0.5 && point=0.5
         const upvoteNumber: number = checking.upvotes + 1;
-        const query = { _id: questionID };
-        const updateDocument = { upvotes: upvoteNumber };
+        const queryUp = { _id: questionID };
+        const updateDocumentUp = { upvotes: upvoteNumber };
+
+        const query = { _id: userId };
+        const updateDocument = { userPoints: point };
 
         const f = await Question.updateOne(
-          query,
-          updateDocument,
+          queryUp,
+          updateDocumentUp,
         );
+
+        await Signup.updateOne(query, updateDocument);
+
+        res.send('200');
+      } else {
+        res.status(200).send('not found');
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  },
+);
+
+QuestionRoute.post(
+  '/downvote/:id',
+  async (req: Request, res: Response) => {
+    const questionID: Types.ObjectId | string =
+      req.params.id;
+    const { userId } = req.body;
+
+    try {
+      const checking:any = await Question.findOne({
+        _id: questionID,
+      });
+
+      const userToFound: any = await Signup.findOne({
+        _id: checking.userID,
+      });
+
+	  console.log(userToFound);
+
+
+      if (checking) {
+        //  point >= 0.5 && point=0.5
+        let point: number = userToFound.userPoints - 0.5;
+        const downvoteNumber: number =
+          checking.downvotes + 1;
+        const queryDown = { _id: questionID };
+        const updateDocumentDown = {
+          downvotes: downvoteNumber,
+        };
+
+        const query = { _id: userToFound._id };
+        const updateDocument = { userPoints: point };
+
+        const f = await Question.updateOne(
+          queryDown,
+          updateDocumentDown,
+        );
+
+        await Signup.updateOne(query, updateDocument);
 
         res.send('200');
       } else {
